2080e7ca48ab412e68d3d1357f01c818f70f28aa
Тимур Комолов <komolov.timurka@mail.ru>
2024-06-15 18:16:50 +0300

    added docstrings

--- main.py
+++ main.py
@@ -21,6 +21,11 @@ model_audio = AudioTagging(checkpoint_path=None, device='cuda' if torch.cuda.is_
 
 
 def make_plt_rows(matrix_l):
+    """
+    Make plot of rows
+    :param matrix_l:
+    :return:
+    """
     points_dict = {}
 
     # Проходимся по строкам матрицы косинусных расстояний
@@ -88,6 +93,11 @@ def make_plt_rows(matrix_l):
 
 
 def make_plt_columns(matrix_l):
+    """
+    Make plot of columns
+    :param matrix_l:
+    :return:
+    """
     points_dict = {}
     for j in range(max(matrix_l.shape[0], matrix_l.shape[1])):
         points_dict[j] = 0
@@ -152,6 +162,13 @@ def make_plt_columns(matrix_l):
 
 
 def get_embeddings_vit(frames, feature_extractor_l, model_l):
+    """
+    Get embeddings from frames using vit transformer
+    :param frames:
+    :param feature_extractor_l: model like ViTFeatureExtractor
+    :param model_l: model like VIT transformer
+    :return:
+    """
     # Преобразуем кадры из формата OpenCV в формат PIL
     images = [Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)) for frame in frames]
     inputs = feature_extractor_l(images=images, return_tensors="pt")
@@ -164,7 +181,15 @@ def get_embeddings_vit(frames, feature_extractor_l, model_l):
     return outputs.last_hidden_state.mean(dim=1)
 
 
-def extract_frame_embeddings_vit(video_path, model_l, feature_extractor_l, frame_interval=None):
+def extract_frame_embeddings_vit(video_path: str, model_l, feature_extractor_l, frame_interval=None) -> np.ndarray:
+    """
+    Extract frame embeddings from video
+    :param video_path: path to the video
+    :param model_l: model like VIT transformer
+    :param feature_extractor_l: model like ViTFeatureExtractor
+    :param frame_interval:
+    :return: embeddings of frames
+    """
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     if frame_interval is None:
@@ -189,13 +214,24 @@ def extract_frame_embeddings_vit(video_path, model_l, feature_extractor_l, frame
     return outputs.detach().cpu().squeeze().numpy()
 
 
-def get_sound_embedding(audio_path, n=10):
+def get_sound_embedding(audio_path: str, n=10) -> np.ndarray:
+    """
+    Extract sound embedding from audio file
+    :param audio_path: path to the audio file
+    :param n:
+    :return: embedding of audio
+    """
     (audio, _) = librosa.core.load(audio_path, sr=44100, mono=True)
     _, emb = model_audio.inference(audio[None, :])
     return np.array([emb[0]] * n)
 
 
-def extract_audio_from_mp4(file_path):
+def extract_audio_from_mp4(file_path: str) -> str:
+    """
+    Extract audio from mp4 file to wav
+    :param file_path: path to the audio file
+    :return: path to the new file
+    """
     video = mp.VideoFileClip(file_path)
     temp_audio_path = "temp_audio.wav"
     video.audio.write_audiofile(temp_audio_path, verbose=False, logger=None)
@@ -203,7 +239,14 @@ def extract_audio_from_mp4(file_path):
     return temp_audio_path
 
 
-def get_video_embeddings(filename, model_l, feature_extractor_l):
+def get_video_embeddings(filename: str, model_l, feature_extractor_l) -> dict:
+    """
+    Get embeddings from video
+    :param filename: name of the file
+    :param model_l: model like VIT transformer
+    :param feature_extractor_l: model like ViTFeatureExtractor
+    :return:
+    """
     video_embeddings_l = np.array([]).reshape(0, 768)
     audio_embeddings_l = np.array([]).reshape(0, 2048)
     segments = []
@@ -236,7 +279,13 @@ def get_video_embeddings(filename, model_l, feature_extractor_l):
             "segments": segments, "filenames": filenames}
 
 
-def create_lance_table(table_name, vector_dim):
+def create_lance_table(table_name: str, vector_dim) -> lancedb.DBConnection:
+    """
+    create vector table
+    :param table_name: name of the table
+    :param vector_dim: dimension of the vector
+    :return: database connection
+    """
     schema = pa.schema([
         ('vector', pa.list_(pa.float64(), vector_dim)),
         ('segment_time', pa.int64()),
@@ -252,7 +301,16 @@ def create_lance_table(table_name, vector_dim):
     return db
 
 
-def append_vector_to_table(db: lancedb.DBConnection, table_name: str, vector, segments, filenames):
+def append_vector_to_table(db: lancedb.DBConnection, table_name: str, vector, segments, filenames) -> None:
+    """
+    append vector of embeddings to table
+    :param db: lancedb connection
+    :param table_name: name of the table
+    :param vector:
+    :param segments:
+    :param filenames:
+    :return:
+    """
     table = db.open_table(table_name)
 
     for i in range(len(segments)):
@@ -263,52 +321,57 @@ def append_vector_to_table(db: lancedb.DBConnection, table_name: str, vector, se
         table.add(t)
 
 
-model = ViTModel.from_pretrained('google/vit-base-patch16-224')
-feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-model.eval()
+def check_similarity():
+    model = ViTModel.from_pretrained('google/vit-base-patch16-224')
+    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
 
-dict_data = get_video_embeddings("ydcrodwtz3mstjq1vhbdflx6kyhj3y0p.mp4",
-                                 model, feature_extractor)
-dict_data_1 = get_video_embeddings("ded3d179001b3f679a0101be95405d2c.mp4",
-                                   model, feature_extractor)
+    dict_data = get_video_embeddings("ydcrodwtz3mstjq1vhbdflx6kyhj3y0p.mp4",
+                                     model, feature_extractor)
+    dict_data_1 = get_video_embeddings("ded3d179001b3f679a0101be95405d2c.mp4",
+                                       model, feature_extractor)
 
-_ = create_lance_table("video_embeddings", len(dict_data_1["video"][0]))
-database = create_lance_table("audio_embeddings", len(dict_data_1["audio"][0]))
+    _ = create_lance_table("video_embeddings", len(dict_data_1["video"][0]))
+    database = create_lance_table("audio_embeddings", len(dict_data_1["audio"][0]))
 
-append_vector_to_table(database, "video_embeddings", dict_data_1["video"],
-                       dict_data_1["segments"], dict_data_1["filenames"])
-append_vector_to_table(database, "audio_embeddings", dict_data_1["audio"],
-                       dict_data_1["segments"], dict_data_1["filenames"])
+    append_vector_to_table(database, "video_embeddings", dict_data_1["video"],
+                           dict_data_1["segments"], dict_data_1["filenames"])
+    append_vector_to_table(database, "audio_embeddings", dict_data_1["audio"],
+                           dict_data_1["segments"], dict_data_1["filenames"])
 
-table_video = database.open_table("video_embeddings")
-table_audio = database.open_table("audio_embeddings")
-threshold_video = 0.6
-threshold_audio = 0.08
-percent_dict = {}
-for batch in dict_data["video"]:
-    result = table_video.search(batch, vector_column_name="vector").metric("cosine").limit(10).to_list()
-    if result[0]["_distance"] < threshold_audio:
-        if not percent_dict.get(result[0]["filename"]):
-            percent_dict[result[0]["filename"]] = 1
-        else:
-            percent_dict[result[0]["filename"]] += 1
-print("-" * 40)
+    table_video = database.open_table("video_embeddings")
+    table_audio = database.open_table("audio_embeddings")
+    threshold_video = 0.6
+    threshold_audio = 0.08
+    percent_dict = {}
+    for batch in dict_data["video"]:
+        result = table_video.search(batch, vector_column_name="vector").metric("cosine").limit(10).to_list()
+        if result[0]["_distance"] < threshold_audio:
+            if not percent_dict.get(result[0]["filename"]):
+                percent_dict[result[0]["filename"]] = 1
+            else:
+                percent_dict[result[0]["filename"]] += 1
+    print("-" * 40)
 
-for batch in dict_data["audio"]:
-    result = table_audio.search(batch, vector_column_name="vector").metric("cosine").limit(10).to_list()
-    if result[0]["_distance"] < threshold_audio:
-        if not percent_dict.get(result[0]["filename"]):
-            percent_dict[result[0]["filename"]] = 1
-        else:
-            percent_dict[result[0]["filename"]] += 1
-for key, _ in percent_dict.items():
-    percent_dict[key] = percent_dict[key] / (len(dict_data["video"] * 2))
-print(percent_dict)
-matrix = cosine_similarity(dict_data["video"], dict_data_1["video"])
-matrix_audio = cosine_similarity(dict_data["audio"], dict_data_1["audio"])
-martix = matrix + matrix_audio
-print(max(percent_dict.items(), key=operator.itemgetter(1))[0])
-make_plt_rows(matrix)
-make_plt_columns(matrix)
+    for batch in dict_data["audio"]:
+        result = table_audio.search(batch, vector_column_name="vector").metric("cosine").limit(10).to_list()
+        if result[0]["_distance"] < threshold_audio:
+            if not percent_dict.get(result[0]["filename"]):
+                percent_dict[result[0]["filename"]] = 1
+            else:
+                percent_dict[result[0]["filename"]] += 1
+    for key, _ in percent_dict.items():
+        percent_dict[key] = percent_dict[key] / (len(dict_data["video"] * 2))
+    print(percent_dict)
+    matrix = cosine_similarity(dict_data["video"], dict_data_1["video"])
+    matrix_audio = cosine_similarity(dict_data["audio"], dict_data_1["audio"])
+    martix = matrix + matrix_audio
+    print(max(percent_dict.items(), key=operator.itemgetter(1))[0])
+    make_plt_rows(matrix)
+    make_plt_columns(matrix)
+
+
+if "__main__" == __name__:
+    check_similarity()
