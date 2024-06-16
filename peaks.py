07888b2a237c51d6c121e9f3060e755cc2f95a9e
Roman Legonkov <rlegonkov@mail.ru>
2024-06-16 11:26:58 +0300

    web

---
+++ peaks.py
@@ -0,0 +1,129 @@
+from scipy.interpolate import PchipInterpolator
+from scipy.signal import find_peaks, peak_widths
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def make_plt_rows(matrix_l, plt_verbose = False):
+    """
+    Make plot of rows
+    :param plt_verbose:
+    :param matrix_l:
+    :return:
+    """
+    points_dict = {}
+
+    for i in range(max(matrix_l.shape[0], matrix_l.shape[1])):
+        points_dict[i] = 0
+
+    for i in range(matrix_l.shape[0]):
+        max_index = np.argmax(matrix_l[i])
+        max_value = matrix_l[i, max_index]
+        points_dict[max_index] = max_value
+
+    x_points = list(points_dict.keys())
+    y_points = list(points_dict.values())
+    y_points = np.maximum(y_points, 0)
+
+    sorted_indices = np.argsort(x_points)
+    x_points = np.take(np.array(x_points), sorted_indices)
+    y_points = np.take(np.array(y_points), sorted_indices)
+
+    def moving_average(data, window_size):
+        return np.convolve(data, np.ones(window_size) / window_size, mode='same')
+
+    window_size = max(int(y_points.shape[0] * 0.05), 3)
+    y_points_smoothed = moving_average(y_points, window_size)
+
+    f_interp = PchipInterpolator(x_points, y_points_smoothed)
+    x_smooth = np.linspace(x_points.min(), x_points.max(), len(x_points))
+    y_smooth = f_interp(x_smooth)
+
+    peaks, _ = find_peaks(y_smooth)
+    widths_half_max = peak_widths(y_smooth, peaks, rel_height=0.50)
+
+    max_peak_idx = np.argmax(y_smooth[peaks])
+    max_peak_height = y_smooth[peaks][max_peak_idx]
+
+    max_width_idx = np.argmax(widths_half_max[0])
+    max_peak_width = widths_half_max[0][max_width_idx]
+    left_ips_x = 0
+    right_ips_x = 0
+    if peaks[max_peak_idx] == peaks[max_width_idx]:
+
+        left_ips_x = x_smooth[int(widths_half_max[2][max_width_idx])]
+        right_ips_x = x_smooth[int(widths_half_max[3][max_width_idx])]
+
+    elif peaks[max_width_idx]:  #and widths_half_max[0][max_peak_idx] > 50:
+        left_ips_x = x_smooth[int(widths_half_max[2][max_peak_idx])]
+        right_ips_x = x_smooth[int(widths_half_max[3][max_peak_idx])]
+    if plt_verbose:
+        plt.plot(x_smooth, y_smooth)
+        plt.xlabel('Index of Minimum Cosine Distance')
+        plt.ylabel('Sum of Max Value - Min Value')
+        plt.title('Graph of Minimum Cosine Distances with Peaks')
+        plt.grid(True)
+        plt.show()
+    return f"{left_ips_x}-{right_ips_x}"
+
+
+def make_plt_columns(matrix_l, plt_verbose = False ):
+    """
+    Make plot of columns
+    :param plt_verbose:
+    :param matrix_l:
+    :return:
+    """
+    points_dict = {}
+    for j in range(max(matrix_l.shape[0], matrix_l.shape[1])):
+        points_dict[j] = 0
+    print(matrix_l.shape[1])
+    for j in range(matrix_l.shape[1]):
+        max_index = np.argmax(matrix_l[:, j])
+        max_value = matrix_l[max_index, j]
+        points_dict[max_index] = max_value
+
+    x_points = list(points_dict.keys())
+    y_points = list(points_dict.values())
+
+    sorted_indices = np.argsort(x_points)
+    x_points = np.take(np.array(x_points), sorted_indices)
+    y_points = np.take(np.array(y_points), sorted_indices)
+
+    def moving_average(data, window_size):
+        return np.convolve(data, np.ones(window_size) / window_size, mode='same')
+
+    window_size = max(int(y_points.shape[0] * 0.05), 3)
+    y_points_smoothed = moving_average(y_points, window_size)
+
+    f_interp = PchipInterpolator(x_points, y_points_smoothed)
+    x_smooth = np.linspace(x_points.min(), x_points.max(), len(x_points))
+    y_smooth = f_interp(x_smooth)
+
+    peaks, _ = find_peaks(y_smooth)
+    widths_half_max = peak_widths(y_smooth, peaks, rel_height=0.50)
+
+    max_peak_idx = np.argmax(y_smooth[peaks])
+    max_peak_height = y_smooth[peaks][max_peak_idx]
+
+    max_width_idx = np.argmax(widths_half_max[0])
+    max_peak_width = widths_half_max[0][max_width_idx]
+    left_ips_x = 0
+    right_ips_x = 0
+    if peaks[max_peak_idx] == peaks[max_width_idx]:
+
+        left_ips_x = x_smooth[int(widths_half_max[2][max_width_idx])]
+        right_ips_x = x_smooth[int(widths_half_max[3][max_width_idx])]
+
+    elif peaks[max_width_idx]:  #and widths_half_max[0][max_peak_idx] > 50#:
+        left_ips_x = x_smooth[int(widths_half_max[2][max_peak_idx])]
+        right_ips_x = x_smooth[int(widths_half_max[3][max_peak_idx])]
+
+    if plt_verbose:
+        plt.plot(x_smooth, y_smooth)
+        plt.xlabel('Index of Minimum Cosine Distance')
+        plt.ylabel('Sum of Max Value - Min Value')
+        plt.title('Graph of Minimum Cosine Distances with Peaks')
+        plt.grid(True)
+        plt.show()
+    return f"{left_ips_x}-{right_ips_x}"
---
+++ web_inference.py
@@ -0,0 +1,125 @@
+import operator
+import tempfile
+import threading
+
+import requests
+from flask import request
+from tqdm import tqdm
+from flask import jsonify
+from transformers import ViTModel, ViTFeatureExtractor
+from panns_inference import AudioTagging
+import torch
+import hashlib
+
+from app import app
+from indexer import *
+from sklearn.metrics.pairwise import cosine_similarity
+from peaks import *
+
+AUDIO_EMBEDDINGS_TABLE = "audio_embeddings"
+AUDIO_EMBEDDINGS_DIM = 2048
+VIDEO_EMBEDDINGS_TABLE = "video_embeddings"
+VIDEO_EMBEDDINGS_DIM = 768
+
+
+def md5_checksum(filepath):
+    hash_md5 = hashlib.md5()
+    with open(filepath, "rb") as file:
+        for chunk in iter(lambda: file.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def run_application():
+    if __name__ == '__main__':
+        threading.Thread(target=lambda: app.run(debug=False)).start()
+
+
+class MainApplication:
+    def __init__(self):
+        self.vit = ViTModel.from_pretrained('google/vit-base-patch16-224')
+        self.feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
+        self.model_audio = AudioTagging(checkpoint_path=None, device='cuda' if torch.cuda.is_available() else 'cpu')
+        self.db = create_lance_table(VIDEO_EMBEDDINGS_TABLE, VIDEO_EMBEDDINGS_DIM)
+        self.db = create_lance_table(AUDIO_EMBEDDINGS_TABLE, AUDIO_EMBEDDINGS_DIM)
+
+    def index_in_db(self, temp_dir):
+        db = get_embeddings_for_directory(temp_dir, self.vit, self.feature_extractor, self.model_audio)
+        return db is not None
+
+    def search_in_db(self, filename):
+        dict_data = get_video_embeddings(filename,
+                                         self.vit, self.feature_extractor, self.model_audio)
+        table_video = self.db.open_table(VIDEO_EMBEDDINGS_TABLE)
+        table_audio = self.db.open_table(AUDIO_EMBEDDINGS_TABLE)
+        threshold_video = 0.6
+        threshold_audio = 0.08
+        percent_dict = {}
+        for batch in dict_data["video"]:
+            result = table_video.search(batch, vector_column_name="vector").metric("cosine").limit(10).to_list()
+            if result[0]["_distance"] < threshold_video:
+                if not percent_dict.get(result[0]["filename"]):
+                    percent_dict[result[0]["filename"]] = 1
+                else:
+                    percent_dict[result[0]["filename"]] += 1
+
+        for batch in dict_data["audio"]:
+            result = table_audio.search(batch, vector_column_name="vector").metric("cosine").limit(10).to_list()
+            if result[0]["_distance"] < threshold_audio:
+                if not percent_dict.get(result[0]["filename"]):
+                    percent_dict[result[0]["filename"]] = 1
+                else:
+                    percent_dict[result[0]["filename"]] += 1
+        for key, _ in percent_dict.items():
+            percent_dict[key] = percent_dict[key] / (len(dict_data["video"] * 2))
+
+        file_piracy = max(percent_dict.items(), key=operator.itemgetter(1))[0]  # add threshold for final result
+        full_embedding_video = table_video.search().where(f"filename = {file_piracy}").to_list()
+        full_embedding_video_vec = [x["vector"] for x in full_embedding_video]
+        full_embedding_audio = table_audio.search().where(f"filename = {file_piracy}").to_list()
+        full_embedding_audio_vec = [x["vector"] for x in full_embedding_audio]
+
+        matrix = cosine_similarity(dict_data["video"], full_embedding_video_vec)
+        matrix_audio = cosine_similarity(dict_data["audio"], full_embedding_audio_vec)
+        matrix = matrix + matrix_audio
+
+        return f"{make_plt_rows(matrix)} {make_plt_columns(matrix)}", file_piracy
+
+
+application = MainApplication()
+
+
+@app.route('/set_video_download', methods=['POST'])
+def set_video_download():
+    temp_dir = tempfile.TemporaryDirectory()
+    download_url = request.json.get("download_url")
+    filename = request.json.get("filename")
+    hashsum_md5 = request.json.get("md5")
+    purpose = request.json.get("purpose")
+    response = requests.get(download_url, stream=True)
+
+    if download_url == "":
+        return jsonify({"error": "download url cant be empty"}), 422
+    with open(os.path.join(temp_dir.name, filename), "wb") as handle:
+        for data in tqdm(response.iter_content()):
+            handle.write(data)
+
+    hashsum = md5_checksum(os.path.join(temp_dir.name, filename))
+    if hashsum != hashsum_md5:
+        return jsonify({"error": "file integrity cant be verified"}), 500
+
+    if purpose not in ["index", "val"]:
+        return jsonify({"error": "purpose can be either index or val"}), 422
+
+    if purpose == "index":
+        result = application.index_in_db(temp_dir)
+        if not result:
+            return jsonify({"error": "error while indexing video"}), 500
+        return jsonify({"indexed": True}) , 200
+    if purpose == "val":
+        result = application.search_in_db(os.path.join(temp_dir.name, filename))
+        if not result:
+            return jsonify({"error": "error while searching video"}), 500
+        return jsonify({"intervals": result[0], "filename": result[1]}), 200
+
+    temp_dir.cleanup()
